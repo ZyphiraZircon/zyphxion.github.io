c5f21c66eef74c4072ab9cb28da9ede2b4bf962e
Mark Escobedo <mark@keystoke.com>
2014-10-27 09:44:09 -0500

    Ninjutsu sim

---
+++ ninjustu/js/scripts.js
@@ -0,0 +1,50 @@
+var ninjutsuApp = angular.module('ninjutsuApp', ['ngRoute']);
+
+ninjutsuApp.config(function ($routeProvider) {
+	$routeProvider
+		.when ('/start',
+		{
+			controller: 'DataController',
+			templateUrl:'partials/Start.html'
+		})
+		.otherwise({redirectTo: '/start' })
+		;
+});
+
+ninjutsuApp.factory('Data', function () {
+	return {message:"I'm data from a service/factory"}
+});
+
+
+ninjutsuApp.controller('DataController', function($scope, Data) {
+	$scope.data = Data;
+	$scope.mudras = [];
+
+	document.onkeyup = function(e) {
+	  if (e.keyCode == 49 ){
+	    $scope.mudras.push('ten');
+	    $scope.checkNinjutsu();
+	  }
+	  else if (e.keyCode == 50 ){
+	    $scope.mudras.push('chi');
+	    $scope.checkNinjutsu();
+	  }
+	  else if (e.keyCode == 51 ){
+	    $scope.mudras.push('jin');
+	    $scope.checkNinjutsu();
+	  }
+	};
+
+	$scope.checkNinjutsu = function(){
+		if ($scope.mudras.length <= 1){
+			//do nothing
+		}
+	};
+
+});
+
+
+
+
+
+
